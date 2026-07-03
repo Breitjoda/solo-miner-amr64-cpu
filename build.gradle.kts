c2daa6d099bda23bf88e3a6be8bf9e04aeac281b
Breitjoda <119367739+Breitjoda@users.noreply.github.com>
2026-07-03 04:40:43 +0200

    Add: Gradle & Project Configuration

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("com.android.library") version "8.1.3" apply false
+    kotlin("android") version "1.9.10" apply false
+    kotlin("jvm") version "1.9.10" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+task("clean") {
+    delete(rootProject.buildDir)
+}
